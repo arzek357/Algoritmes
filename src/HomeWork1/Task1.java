fa3f263b40dd7be67d1b0b58690ca995acbb4384
arzek357 <arzek357@gmail.com>
2020-02-08 12:18:01 +0300

    Change array numbers

--- src/HomeWork1/Task1.java
+++ src/HomeWork1/Task1.java
@@ -4,6 +4,18 @@ import java.sql.SQLOutput;
 
 public class Task1 {
     public static void main(String[] args) {
-        int[] arr = {1,0,3,4,-1,-8,-9,-5};
+        int[] arr = {1,0,-2,7,1,8,-3,-2,-1,-7};
+        var1(arr);
+    }
+    //Вариант 1
+    private static void var1(int[] arr){
+        for (int i =0;i<arr.length;i++){
+            for (int j =0;j<arr.length;j++){
+                for (int k =0;k<arr.length;k++){
+                    if (arr[i]+arr[j]+arr[k]==0)
+                        System.out.println(arr[i]+" "+arr[j]+" "+arr[k]);
+                }
+            }
+        }
     }
 }
