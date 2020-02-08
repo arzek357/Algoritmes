8fee58d8dd487f12db41ca53eb25948dc60f448b
arzek357 <arzek357@gmail.com>
2020-02-08 12:11:44 +0300

    Change array numbers

--- src/HomeWork1/Task2.java
+++ src/HomeWork1/Task2.java
@@ -2,7 +2,7 @@ package HomeWork1;
 
 public class Task2 {
     public static void main(String[] args) {
-        int[] arr = {1,8};
+        int[] arr = {1,0,-2,7,1,8,-3,-2,-1,-7};
         var1(arr);
     }
     //Вариант 1 (Сложность: N)
